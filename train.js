3b001e367f036564091ccfcf8db22a75590daa7d
michael <abdulazizklichev1@gmail.com>
2024-02-10 01:06:53 +0900

    TASK_A :Callback&& Async

--- train.js
+++ train.js
@@ -1,32 +1,62 @@
-console.log("Jack Ma maslahatlari");
-const list = [
-    "yaxshi talaba buling", //0-20
-    "tugri boshliq tanlang va koproq xato qiling", //20-30
-    "uzingizga ishlashingizni boshlang", //30-40
-    "siz kuchli bolgan narsalarni qiling", //40-50
-    "yoshlarga investitsiya qiling", //50-60
-    "endi dam oling, foydasi yuq endi",//60
-]; //Callback function
-function maslahatBering(a, callback){
-    if(typeof a !== 'number') callback("insert  a number",null);
-    else if(a<=20) callback(null, list[0]);
-    else if(a >20 && a <= 30) callback(null,list[1]);
-    else if(a >30 && a <= 40) callback(null,list[2]);
-    else if(a >40 && a <= 50) callback(null,list[3]);
-    else if(a >50 && a <= 60) callback(null,list[4]);
-  else{
-    setInterval(() => {
-      callback(null, list[5]);
-    }, 1000);
+// TASK A 
+// Shunday 2 parametrli function tuzing,
+// hamda birinchi parametrdagi leterni ikkinchi parametrdagi suzdan
+// qatnashgan sonni return qilishi kerak buladi
+// Masalan countLetter("e","engineer") 3ni return qiladi
+const suz = (alp,word)=>{
+  let count = 0;
+  for(let i = 0; i<word.length;i++){
+  if(word[i]===alp){
+    count++;
   }
 }
-console.log("Passed here 0");
-maslahatBering(65, (err, data)=>{
-    if(err) console.log('ERROR:', err);
-    else{ 
-        console.log('javob:',data);}
-});
-console.log("passed here 1");
+  return count;
+};
+
+const result = suz("b", "basketbol");
+console.log(result);
+
+//             2- uslub
+// const lett = (let,word)=>{ 
+//   const letterword = word.split('');
+//   const wordletter = letterword.filter((element)=>{
+//     return element===let;
+//   });
+//   const countLetter = wordletter.length;
+//   return countLetter; 
+// }
+// const result = lett("a","assotsatsiya");
+// console.log(result);
+//************************************************** */
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//     "yaxshi talaba buling", //0-20
+//     "tugri boshliq tanlang va koproq xato qiling", //20-30
+//     "uzingizga ishlashingizni boshlang", //30-40
+//     "siz kuchli bolgan narsalarni qiling", //40-50
+//     "yoshlarga investitsiya qiling", //50-60
+//     "endi dam oling, foydasi yuq endi",//60
+// ]; //Callback function
+// function maslahatBering(a, callback){
+//     if(typeof a !== 'number') callback("insert  a number",null);
+//     else if(a<=20) callback(null, list[0]);
+//     else if(a >20 && a <= 30) callback(null,list[1]);
+//     else if(a >30 && a <= 40) callback(null,list[2]);
+//     else if(a >40 && a <= 50) callback(null,list[3]);
+//     else if(a >50 && a <= 60) callback(null,list[4]);
+//   else{
+//     setInterval(() => {
+//       callback(null, list[5]);
+//     }, 5000);
+//   }
+// }
+// console.log("Passed here 0");
+// maslahatBering(65, (err, data)=>{
+//     if(err) console.log('ERROR:', err);
+//     else{ 
+//         console.log('javob:',data);}
+// });
+// console.log("passed here 1");
 
 
 
