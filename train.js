a6f78002a4da26622b2dfdfe80a8a31f5950a135
michael <abdulazizklichev1@gmail.com>
2024-02-19 21:34:01 +0900

    fix:Task-E

--- train.js
+++ train.js
@@ -1,4 +1,9 @@
+//Task-E
+// Shunday function tuzing, u bitta string argumentni qabul qilib osha stringni teskari qilib return qilsin.
+// MASALAN: getReverse("hello") return qilsin "olleh"
+
 function str (a){
-  return  a.split('').reverse().join('');
+const b = a.split('').reverse().join('');
+console.log(b);
 }
-console.log(str('michael'));
+str('AssalomuAleykum');
