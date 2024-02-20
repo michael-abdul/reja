caae35cf0df97665f2b3a79ec988f760ae0e0e06
michael <abdulazizklichev1@gmail.com>
2024-02-21 06:54:32 +0900

    fix:TASK-F

--- train.js
+++ train.js
@@ -1,9 +1,56 @@
+// F-TASK: 
+
+// Shunday findDoublers function tuzing,
+// unga faqat bitta string argument pass bolib,
+//  agar stringda bir hil harf qatnashgan bolsa true, qatnashmasa false qaytarishi kerak.
+// MASALAN: getReverse("hello") return true return qiladi
+function findDoublers(str) {
+     const a = str.split('').some((total, ele, arr) => total == arr[ele+1]);
+ console.log(a)
+}
+
+findDoublers('daddy');
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //Task-E
 // Shunday function tuzing, u bitta string argumentni qabul qilib osha stringni teskari qilib return qilsin.
 // MASALAN: getReverse("hello") return qilsin "olleh"
 
-function str (a){
-const b = a.split('').reverse().join('');
-console.log(b);
-}
-str('AssalomuAleykum');
+// function str (a){
+// const b = a.split('').reverse().join('');
+// console.log(b);
+// }
+// str('AssalomuAleykum');
