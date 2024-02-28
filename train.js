bf5ae0939d50b26dde4c3bb8f260c5d0c4e17ead
michael <abdulazizklichev1@gamil.com>
2024-02-28 09:59:29 +0900

    nuqta qushildi

--- train.js
+++ train.js
@@ -2,7 +2,7 @@
 /*G-TASK: 
 
 Shunday function tuzingki unga integerlardan iborat array pass bolsin va function bizga osha arrayning eng katta qiymatiga tegishli birinchi indexni qaytarsin.
-MASALAN: getHighestIndex([5, 21, 12, 21, 8]) return qiladi 1 sonini.
+MASALAN: getHighestIndex([5, 21, 12, 21, 8]) return qiladi 1 sonini........
 
 
 */
